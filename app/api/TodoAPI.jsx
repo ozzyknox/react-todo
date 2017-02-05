37c77ea2c3ba464c00bad31694461b44cfbd0b96
ozzyawaken <ozzy.knox@awakensoftware.com>
2017-02-05 23:58:32 +0100

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -25,8 +25,24 @@ module.exports = {
         });
 
         // Filter by searchText
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
 
         // Sort todos with non-completed first
+        filteredTodos = filteredTodos.filter((todo) => {
+            if (searchText.length === 0) {
+                return true;
+            } else {
+                return todo.text.toLowerCase().includes(searchText.toLowerCase());
+            }
+        });
 
         return filteredTodos;
     }
