6747c8b76de51b39ee9254d656d735c2491c79a7
ozzyawaken <ozzy.knox@awakensoftware.com>
2017-02-08 19:59:28 +0100

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,43 @@ describe('Reducers', () => {
             expect(res).toEqual(true);
         });
     });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'Walk the dog'
+            };
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should toggle todo', () => {
+            var todos = [{
+                id: 11,
+                text: 'Test features',
+                completed: false,
+                createdAt: 60,
+                completedAt: undefined
+            }];
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 11
+            };
+            var res = reducers.todosReducer(df(todos), df(action));
+
+            expect(res[0].completed).toEqual(true);
+            expect(res[0].completedAt).toBeGreaterThan(0);
+
+            // action = {
+            //     type: 'TOGGLE_TODO',
+            //     id: 11
+            // };
+            // res = reducers.todosReducer(df(todos), df(action));
+            // expect(res[0].completed).toEqual(false);
+            // expect(res[0].completedAt).toEqual(undefined);
+        });
+    });
 });
