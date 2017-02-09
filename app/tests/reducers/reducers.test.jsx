a93847b56f1d25bc50a117c69afcea002e4ce001
ozzyawaken <ozzy.knox@awakensoftware.com>
2017-02-09 14:15:21 +0100

    Update todos on firebase

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -44,7 +44,7 @@ describe('Reducers', () => {
             expect(res[0]).toEqual(action.todo);
         });
 
-        it('should toggle todo', () => {
+        it('should update todo', () => {
             var todos = [{
                 id: 11,
                 text: 'Test features',
@@ -52,14 +52,20 @@ describe('Reducers', () => {
                 createdAt: 60,
                 completedAt: undefined
             }];
+            var updates = {
+                completed: false,
+                completedAt: null
+            };
             var action = {
-                type: 'TOGGLE_TODO',
-                id: 11
+                type: 'UPDATE_TODO',
+                id: todos[0].id,
+                updates
             };
             var res = reducers.todosReducer(df(todos), df(action));
 
-            expect(res[0].completed).toEqual(true);
-            expect(res[0].completedAt).toBeGreaterThan(0);
+            expect(res[0].completed).toEqual(updates.completed);
+            expect(res[0].completedAt).toEqual(updates.completedAt);
+            expect(res[0].text).toEqual(todos[0].text);
         });
 
         it('should add existing todos', () => {
